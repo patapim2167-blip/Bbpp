77b87ca259e2d41aa958f85840a8a1deba0a8923
patapim2167-blip <patapim2167@gmail.com>
2026-06-23 14:21:19 +0700

    app.js

--- app.js
+++ app.js
@@ -1,34 +1,203 @@
-function showResult(phone,data){
+const result=document.getElementById("result");
+
+async function analyzePhone(){
+
+const phone=document
+.getElementById("phone")
+.value
+.trim();
+
+if(!phone){
 
 result.innerHTML=`
 
-<div class="result-card">📱 ${phone}</div>
+<div class="result-card">
 
-<div class="result-card">🎯 Độ nhạy đề xuất<br><br>
+⚠️ Hãy nhập tên điện thoại
 
-Tổng quan: 190
+</div>
 
-<br>Red Dot:190
+`;
 
-<br>2x:180
+return;
 
-<br>4x:170
+}
 
-<br>AWM:80
+try{
 
-<br>Nhìn xung quanh:200
+const files=[
+
+"samsung.json",
+
+"xiaomi.json",
+
+"iphone.json"
+
+];
+
+let database={};
+
+for(const file of files){
+
+const response=
+
+await fetch(file);
+
+const data=
+
+await response.json();
+
+database={
+
+...database,
+
+...data
+
+};
+
+}
+
+const data=
+
+database[phone];
+
+if(!data){
+
+result.innerHTML=`
+
+<div class="result-card">
+
+❌ Chưa có dữ liệu của ${phone}
+
+</div>
+
+`;
+
+return;
+
+}
+
+showResult(phone,data);
+
+}
+
+catch(e){
+
+result.innerHTML=`
+
+<div class="result-card">
+
+❌ Lỗi đọc file JSON
+
+</div>
+
+`;
+
+}
+
+}
+
+function showResult(phone,data){
+
+let general=190;
+
+let redDot=185;
+
+let scope2=175;
+
+let scope4=165;
+
+let awm=80;
+
+let freeLook=200;
+
+let dpi=520;
+
+let fire=56;
+
+if(data.hz>=120){
+
+general=195;
+
+redDot=190;
+
+scope2=180;
+
+scope4=170;
+
+awm=85;
+
+dpi=560;
+
+fire=52;
+
+}
+
+result.innerHTML=`
+
+<div class="result-card">
+
+📱 ${phone}
 
 </div>
 
 <div class="result-card">
 
-🖱️ DPI: 520
+🧠 CPU: ${data.cpu}
 
 </div>
 
 <div class="result-card">
 
-🔘 Nút bắn: 56%
+💾 RAM: ${data.ram}GB
+
+</div>
+
+<div class="result-card">
+
+📺 ${data.hz}Hz
+
+</div>
+
+<div class="result-card">
+
+🎯 Độ nhạy đề xuất
+
+<br><br>
+
+Tổng quan: ${general}
+
+<br>
+
+Red Dot: ${redDot}
+
+<br>
+
+2X: ${scope2}
+
+<br>
+
+4X: ${scope4}
+
+<br>
+
+AWM: ${awm}
+
+<br>
+
+Nhìn xung quanh: ${freeLook}
+
+</div>
+
+<div class="result-card">
+
+🖱️ DPI: ${dpi}
+
+</div>
+
+<div class="result-card">
+
+🔘 Nút bắn: ${fire}%
 
 </div>
 
@@ -46,7 +215,7 @@ FPS: Cao
 
 <br>
 
-Độ phân giải: Tiêu chuẩn
+Bóng đổ: Tắt
 
 </div>
 
@@ -56,15 +225,15 @@ FPS: Cao
 
 <br><br>
 
-🌡️ Tắt RAM Plus
-
-<br>
-
 🔕 Không làm phiền: Bật
 
 <br>
 
 🔋 Tắt tiết kiệm pin
+
+<br>
+
+🧹 Đóng ứng dụng nền
 
 </div>
 
