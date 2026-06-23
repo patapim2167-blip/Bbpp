43edc48d30e5825685245393164f94c791f8fa36
patapim2167-blip <patapim2167@gmail.com>
2026-06-23 13:31:32 +0700

    app.js

--- app.js
+++ app.js
@@ -1,28 +1,5 @@
 function showResult(phone,data){
 
-let dpi=520;
-let fire=56;
-
-let general=190;
-let redDot=185;
-let scope2=175;
-let scope4=160;
-let awm=80;
-let freeLook=200;
-
-if(data.hz>=120){
-
-dpi=560;
-fire=52;
-
-general=195;
-redDot=190;
-scope2=180;
-scope4=170;
-awm=85;
-
-}
-
 result.innerHTML=`
 
 <div class="result-card">
@@ -39,95 +16,25 @@ result.innerHTML=`
 
 <div class="result-card">
 
-💾 RAM: ${data.ram}GB
+💾 RAM: ${data.ram}
 
 </div>
 
 <div class="result-card">
 
-📺 Màn hình: ${data.hz}Hz
+📺 Màn hình: ${data.hz}
 
 </div>
 
 <div class="result-card">
 
-🎯 Độ nhạy đề xuất
-
-<br><br>
-
-Tổng quan: ${general}
-
-<br>
-
-Red Dot: ${redDot}
-
-<br>
-
-2x: ${scope2}
-
-<br>
-
-4x: ${scope4}
-
-<br>
-
-AWM: ${awm}
-
-<br>
-
-Nhìn xung quanh: ${freeLook}
+🎯 Cấu hình được đề xuất theo thiết bị.
 
 </div>
 
 <div class="result-card">
 
-🖱️ DPI: ${dpi}
-
-</div>
-
-<div class="result-card">
-
-🔘 Nút bắn: ${fire}%
-
-</div>
-
-<div class="result-card">
-
-🎮 Setting game
-
-<br><br>
-
-Đồ họa: Mượt
-
-<br>
-
-FPS: Cao
-
-<br>
-
-Độ phân giải: Tiêu chuẩn
-
-</div>
-
-<div class="result-card">
-
-📱 Setting máy
-
-<br><br>
-
-🔋 Tiết kiệm pin: Tắt
-
-<br>
-
-🌙 Độ sáng: 75%
-
-<br>
-
-🔕 Không làm phiền: Bật
-
-<br>
-
-🧹 Đóng ứng dụng nền trước khi chơi
+⚙️ Bạn có thể mở rộng dữ liệu sau này.
 
 </div>
 
